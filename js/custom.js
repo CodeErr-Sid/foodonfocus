66c4131b6c80708a44a4fc4e7d69411461dd8fe5
Siddique <siddiqueofl@gmail.com>
2024-05-08 14:52:29 +0530

    tes3

--- js/custom.js
+++ js/custom.js
@@ -56,3 +56,37 @@ document.addEventListener("DOMContentLoaded", function() {
 window.addEventListener("unload", function() {
     sessionStorage.removeItem("cookieConsentStatus");
 });
+
+
+   // Function to check and show the popup only once
+   function showPopupOnce() {
+    var popupDisplayed = getCookie("popupDisplayed");
+    if (!popupDisplayed) {
+        $("#popup").show();
+        setCookie("popupDisplayed", true, 1); // Set cookie to expire in 1 day
+    }
+}
+});
+
+// Function to set cookie
+function setCookie(name, value, days) {
+var expires = "";
+if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+}
+document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+// Function to get cookie
+function getCookie(name) {
+var nameEQ = name + "=";
+var ca = document.cookie.split(';');
+for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+}
+return null;
+}
